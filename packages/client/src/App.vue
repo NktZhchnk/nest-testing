889b088b0ac800e57485ac34f89cea54dc45b766
fedichkin <dev.fedichkin@gmail.com>
2022-09-05 10:15:35 +0300

    init client

---
+++ packages/client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <font-awesome-icon icon="times"/>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  mounted() {
+    axios.post('/auth/login', {
+      username: 'john',
+      password: '0000',
+    }).then((response) => {
+
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
